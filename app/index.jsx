37a03d9215d58322d4fed3f599cc14d26220f0b0
bunokent <bunokent19@gmail.com>
2026-03-14 16:15:59 +0800

    rewrite the stylesheet to nativewind in the index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,16 @@
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
-import { useFonts, Poppins_500Medium, Poppins_400Regular } from '@expo-google-fonts/poppins'
-import { Ionicons } from '@expo/vector-icons';
-import { Link } from 'expo-router'
+import { Text, View, Image, Pressable } from "react-native";
+import {
+  useFonts,
+  Poppins_500Medium,
+  Poppins_400Regular,
+} from "@expo-google-fonts/poppins";
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 const index = () => {
   const [fontsLoaded] = useFonts({
     Poppins_500Medium,
-    Poppins_400Regular
+    Poppins_400Regular,
   });
 
   if (!fontsLoaded) {
@@ -14,30 +18,48 @@ const index = () => {
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#E4DFFD' }}>
-      <Link href="/about" style={{ position: 'absolute', top: 60, right: 30 }}>
+    <View className="flex-1 items-center justify-center bg-[#E4DFFD]">
+      <Link href="/about" className="absolute top-[60px] right-[30px]">
         <Ionicons name="information-circle-outline" size={24} />
       </Link>
-      <Image source={require('../assets/logo.png')} style={{ width: 150, height: 150 }} />
-      <Text style={{ fontSize: 36, fontFamily: 'Poppins_500Medium' }}>WedSnap</Text>
-      <Text style={{ fontSize: 18, fontFamily: 'Poppins_400Regular' }}>The Snap of a Lifetime</Text>
-      
-      <View style={{ flexDirection: 'column', marginTop: 100, width: '80%', gap: 20}}>
-        <Link href='/login' asChild>
-          <Pressable style={{ backgroundColor: '#6A4C93', padding: 14, borderRadius: 10, alignItems: 'center', width: '100%'}}>
-            <Text style={{ color: 'white', fontFamily: 'Poppins_500Medium'}}>Login</Text>
+      <Image
+        source={require("../assets/logo.png")}
+        style={{ width: 150, height: 150 }}
+      />
+      <Text className="text-[36px]" style={{ fontFamily: "Poppins_500Medium" }}>
+        WedSnap
+      </Text>
+      <Text
+        className="text-[18px]"
+        style={{ fontFamily: "Poppins_400Regular" }}
+      >
+        The Snap of a Lifetime
+      </Text>
+
+      <View className="mt-[100px] w-4/5 flex-col gap-5">
+        <Link href="/login" asChild>
+          <Pressable className="w-full items-center rounded-[10px] bg-[#6A4C93] p-[14px]">
+            <Text
+              className="text-white"
+              style={{ fontFamily: "Poppins_500Medium" }}
+            >
+              Login
+            </Text>
           </Pressable>
         </Link>
-        <Link href='/signup' asChild>
-          <Pressable style={{ backgroundColor: '#D4AF37', padding: 14, borderRadius: 10, alignItems: 'center', width: '100%'}}>
-            <Text style={{ color: 'white', fontFamily: 'Poppins_500Medium'}}>Sign Up</Text>
+        <Link href="/signup" asChild>
+          <Pressable className="w-full items-center rounded-[10px] bg-[#D4AF37] p-[14px]">
+            <Text
+              className="text-white"
+              style={{ fontFamily: "Poppins_500Medium" }}
+            >
+              Sign Up
+            </Text>
           </Pressable>
         </Link>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default index
-
-const styles = StyleSheet.create({})
+export default index;
